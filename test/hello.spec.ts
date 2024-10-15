604aa6c900bb782bdb1d2e19c6544ee2a6e7f9c7
Jonas Leite <jonas.programador01@gmail.com>
2024-10-15 07:31:32 -0300

    :recycle: refactor: removing the try and catch for tests

--- test/hello.spec.ts
+++ test/hello.spec.ts
@@ -3,17 +3,13 @@ import axios from 'axios';
 
 describe('Tests some API Graphql ', () => {
   it('Should return a greeting message', async () => {
-    try {
-      const response = await axios.post('http://localhost:4000/graphql', {
-        query: `
+    const response = await axios.post('http://localhost:4000/graphql', {
+      query: `
           query {
             hello
           }
         `,
-      });
-      expect(response.data.data.hello).to.equal('Hello, World!');
-    } catch (error) {
-      console.error('Error executing the test', error);
-    }
+    });
+    expect(response.data.data.hello).to.equal('Hello, World!');
   });
 });
